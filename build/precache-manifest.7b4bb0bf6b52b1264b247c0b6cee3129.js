7094c5ecda3d199df0bbac0e5fd09ca14b441cf9
Sudipta <sudiptarumu@gmail.com>
2019-10-22 13:08:00 +0600

    modified

---
+++ build/precache-manifest.7b4bb0bf6b52b1264b247c0b6cee3129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb03750c177f78736694e16cbc90d72",
+    "url": "/Simple-React-Test-App/index.html"
+  },
+  {
+    "revision": "8f3ee91700e82dcfeded",
+    "url": "/Simple-React-Test-App/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "39bcd460baf778fec3d1",
+    "url": "/Simple-React-Test-App/static/css/main.9e158f36.chunk.css"
+  },
+  {
+    "revision": "8f3ee91700e82dcfeded",
+    "url": "/Simple-React-Test-App/static/js/2.c7724c2d.chunk.js"
+  },
+  {
+    "revision": "39bcd460baf778fec3d1",
+    "url": "/Simple-React-Test-App/static/js/main.a0c2b074.chunk.js"
+  },
+  {
+    "revision": "fdc55f751b4902fe53eb",
+    "url": "/Simple-React-Test-App/static/js/runtime-main.027ded20.js"
+  }
+]);
